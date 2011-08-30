0ab96186f2838803f492d95332be33f2975ad992
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2011-08-30 19:44:13 +0200

    - added *experimential* rx.angular bindings - use Rx to show the "hit enter to save task" hint only after the user *stopped typing* and not *while he is typing*

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -60,9 +60,15 @@ App.Controllers.TodoController =function () {
     return self.todos.length > 0;  
   };
   
-  self.showHitEnterHint = function(){
-    return self.newTodo.length > 0;  
-  };
-  
+   /*
+        The following code deals with hiding the hint *while* you are typing,
+        showing it once you did *finish* righting (aka 500 ms since you hit the last key)
+        *incase* the result is a non empty string 
+   */
+   Rx.Observable.FromAngularScope(self, "newTodo")
+               .Do(function(){ self.showHitEnterHint = false; })
+               .Throttle(500)
+               .Select(function(x){ return x.length > 0; })
+               .ToOutputProperty(self, "showHitEnterHint");
 }
 App.Controllers.TodoController.$inject = [];
---
+++ app/lib/rx/rx.angular.js
@@ -0,0 +1,62 @@
+(function () {
+    var global = this,
+        root = (typeof ProvideCustomRxRootObject == "undefined") ? global.Rx : ProvideCustomRxRootObject();
+
+    var observable = root.Observable;
+    var asyncSubject = root.AsyncSubject;
+    var observableCreate = observable.Create;
+    var disposableEmpty = root.Disposable.Empty;
+
+    if (!Object.getPrototypeOf) {
+        Object.getPrototypeOf = function getPrototypeOf(object) {
+            return object.__proto__ || object.constructor.prototype;
+            // or undefined if not available in this engine
+        };
+    }
+
+/*
+    //it seems as if we have no anguar scope prototype we can extend :-( 
+ 
+    Object.getPrototypeOf(angular.scope).toObservable = function (property) {
+        var angularScope = this;
+        return observableCreate(function (observer) {
+            angularScope.$watch(function(){
+              return angularScope[property];
+            }, function(){
+              observer.OnNext(angularScope[property]);
+            });
+            return function () {
+                // make something to unwatch the expression
+            };
+        });
+    };
+*/
+
+    observable.FromAngularScope = function (angularScope, propertyName) {
+        return observableCreate(function (observer) {
+            var onNext = function(){
+                observer.OnNext(angularScope[propertyName]);
+            };
+            angularScope.$watch(function(){
+              return angularScope[propertyName];
+            }, 
+            onNext, 
+            function(){
+                observer.OnError(angularScope[propertyName]);
+            }, false);
+            return function () {
+               // since AngularJS has no API to unbind we set the previous registered
+               // callback to a empty dummy function (far from perfect!)
+               onNext = function() {};
+            };
+        });
+    };
+
+    root.Observable.prototype.ToOutputProperty = function (scope, propertyName) {
+        var disposable = this.Subscribe(function (data) {
+            scope[propertyName] = data;
+            scope.$eval();
+        });
+    };
+
+})();
