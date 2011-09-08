7694cb0afd17225945edc8144302465e96e44991
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2011-09-08 17:32:00 +0200

    fixed indentation issues

--- app/lib/rx/rx.angular.js
+++ app/lib/rx/rx.angular.js
@@ -14,35 +14,16 @@
         };
     }
 
-/*
-    //it seems as if we have no anguar scope prototype we can extend :-( 
- 
-    Object.getPrototypeOf(angular.scope).toObservable = function (property) {
-        var angularScope = this;
-        return observableCreate(function (observer) {
-            var unwatch angularScope.$watch(function(){
-              return angularScope[property];
-            }, function(){
-              observer.OnNext(angularScope[property]);
-            });
-            return function () {
-                unwatch()
-            };
-        })
-        .Skip(1);
-    };
-*/
-
     observable.FromAngularScope = function (angularScope, propertyName) {
         return observableCreate(function (observer) {
             var unwatch = angularScope.$watch(function(){
-              return angularScope[propertyName];
+                return angularScope[propertyName];
             }, 
             function(){
                 observer.OnNext(angularScope[propertyName]);
             });
             return function () {
-               unwatch();
+                unwatch();
             };
         })
         .Skip(1); //In AngularJS 0.10.x There is no way to avoid initial evaluation. So we take care about it!
@@ -55,18 +36,18 @@
         });
         
         scope.$on('$destroy', function(event){
-          //we need to asure that we only dispose the observable when it's our scope that
-          //was destroyed.
-          
-          //TODO: Figure out if thats enough to asure the above (e.g what happens when
-          //a child scope will be destroyed but ours won't be affected. Or the other way around, 
-          //if a higher scope will be destroyed (and therefore ours as well) does it mean that $destroy() 
-          //will be also called on our scope or will our scope get destroyed without actually 
-          //calling $destroy() on it?
-          if (event.targetScope === scope){
-            disposable.Dispose();
-          }          
-        });        
+            //we need to asure that we only dispose the observable when it's our scope that
+            //was destroyed.
+
+            //TODO: Figure out if thats enough to asure the above (e.g what happens when
+            //a child scope will be destroyed but ours won't be affected. Or the other way around, 
+            //if a higher scope will be destroyed (and therefore ours as well) does it mean that $destroy() 
+            //will be also called on our scope or will our scope get destroyed without actually 
+            //calling $destroy() on it?
+            if (event.targetScope === scope){
+                disposable.Dispose();
+            }
+        });
     };
 
 })();
