7c99efc1738ab294b2c70b6a07ee7ed9ed39781f
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2011-11-19 18:51:12 +0100

    copied over the latest from the master branch

--- js/services.js
+++ js/services.js
@@ -7,22 +7,12 @@ angular.service('myAngularApp', function($route, $location, $window) {
 
   $route.when('/main', {template: 'partials/main.html', controller: App.Controllers.TodoController});
   $route.when('/credits', {template: 'partials/credits.html'});
-  $route.when('', {redirectTo: '/main'});
+  $route.otherwise({redirectTo: '/main'});
 
-/*
   var self = this;
 
-  //Figure out if we actually need any of this any longer in 0.10.x
-
-  $route.$on('$afterRouteChange', function() {
-    if ($location.hash === '') {
-      $location.updateHash('/main');
-      self.$eval();
-    } else {
-      $route.current.scope.params = $route.current.params;
-      $window.scrollTo(0,0);
-    }
+  self.$on('$afterRouteChange', function(){
+    $window.scrollTo(0,0);
   });
-*/
 
 }, {$inject:['$route', '$location', '$window'], $eager: true});
